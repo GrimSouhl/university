c3129d67b66d94eebbe3b479aed6e1e993a9b5a6
Grim <114767081+GrimSouhl@users.noreply.github.com>
2024-12-26 18:07:47 +0100

    fix en CopiaYLlamaMetodo.java

--- PL/Compilador/PLXMINICORE/src/CopiaYLlamaMetodo.java
+++ PL/Compilador/PLXMINICORE/src/CopiaYLlamaMetodo.java
@@ -1,27 +1,28 @@
-
-import jdk.dynalink.linker.MethodTypeConversionStrategy;
-
 public class CopiaYLlamaMetodo extends Instruccion{
 
     private Instruccion e1, param[];
     private String metodo;
 
-    @Overridepublic Objeto generarCodigo() throws Exception {
+    @Override
+    public Objeto generarCodigo() throws Exception {
         Objeto oParam[] = null;
         Objeto r;
 
         Variable o1 = (Variable) e1.generarCodigo();
 
-        r = new Variable( Objeto.newNombObj(), o1.getBloque(),true, o1.get)
-        r.generarCodigoMetodo(MethodTypeConversionStrategy.CONSTRUCTORCOPIA, 
-        new Objeto[] {o1})
+        r = new Variable( Objeto.newNombObj(), o1.getBloque(),true, o1.getTipo());
+        r.generarCodigoMetodo(Metodo.CONSTRUCTORCOPIA, new Objeto[] {o1});
 
         if(param!= null){
             oParam = new Objeto[param.length];
 
             for( int k= 0; k<param.length; k++){
-                
+                oParam[k] = param[k].generarCodigo();
             }
         }
+
+        r.generarCodigoMetodo(metodo,oParam)
+
+            return r;
     }
 }
