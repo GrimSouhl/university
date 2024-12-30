aa4ecc9f5e30e90195344ae7fa446ad8658fec8b
Ana Martín <114767081+GrimSouhl@users.noreply.github.com>
2024-12-30 16:03:45 +0100

    rel2 continuacion

--- Estructura de Datos/Relacion de ejercicios/Rel2.hs
+++ Estructura de Datos/Relacion de ejercicios/Rel2.hs
@@ -128,7 +128,7 @@ divideA n x
         |otherwise = False
 
 divisores ::  Int -> [Int]
-divisores n = [x| x <- [1..n], divideA n x ]
+divisores n = [x| x <- [1..abs n], divideA n x ]
 
 --Haz las modificaciones necesarias para obtener una nueva función que devuelva los
 --divisores (positivos y negativos) de un número entero:
@@ -152,11 +152,17 @@ divisores' n = [x| x <- [-n..n], divideA n x ]
 --través, por ejemplo, de la función predefinida que devuelve el mayor elemento de una lista:
 
 
+divisoresComunes :: Int -> Int -> Int
+divisoresComunes x y 
+                  |null [z| z <- divisores x, divideA y z] = 0  
+                  |otherwise = maximum [z| z <- divisores x, divideA y z]
 
 --b) Define y comprueba, usando QuickCheck, la siguiente propiedad:
 --para x,y,z enteros, con x≠0, y≠0, z≠0, se verifica mcd(z*x, z*y) = |z| mcd(x,y)
 
-
+p_divisoresComunes x y z = ( x/=0 && y/=0 && z/=0 ) ==> divisoresComunes (z*x) (z*y) == ((abs z) * divisoresComunes x y)
+--OK, passed 100 tests; 16 discarded.
+ 
 --c) A partir de la siguiente propiedad que relaciona el mcd con el mcm (mínimo común múltiplo) de dos
 --números
 --mcd x y ∙ mcm x y = x ∙ y
@@ -164,10 +170,85 @@ divisores' n = [x| x <- [-n..n], divideA n x ]
 --N--ota: el algoritmo de Euclides para el cálculo del mcd es más eficiente que el que has desarrollado
 ---e-n este ejercicio.
 
-
+mcm :: Int -> Int -> Int
+mcm x y 
+      |x==0 || y==0 = 0
+      |otherwise= (x*y) `div` (divisoresComunes x y)
 
 --------------------------------------------------------------------------------------------------------
 --8. Un número natural p es primo si tiene exactamente dos divisores positivos distintos: 1 y p; por
 --tanto, 1 no es un número primo.
 
 --a) Define una función para comprobar si un número es primo. Por ejemplo:
+
+esPrimo :: Int -> Bool
+esPrimo n = length (divisores n) == 2
+
+--b) Usando una lista por comprensión, define una función que devuelva una lista
+--con todos los números primos menores o iguales a un valor dado. Por ejemplo:
+
+primosHasta :: Int -> [Int]
+primosHasta n = [x| x <- [1..n], esPrimo x]
+
+
+--c) Da otra definición (con nombre ) que use la función predefinida en vez
+--de la lista por comprensión.
+
+primosHasta' :: Int -> [Int]
+primosHasta' n = filter esPrimo [1..n]
+
+
+
+--d) Comprueba que las dos funciones que has definido se comportan del mismo modo,
+--comprobando con QuickCheck la siguiente propiedad:
+--Nota: existen métodos más eficientes para calcular listas de primos, como la Criba de Eratóstenes.
+
+p1_primos x = primosHasta x == primosHasta' x
+--OK, passed 100 tests.
+
+
+--------------------------------------------------------------------------------------------------------
+--9. La conjetura de Goldbach fue formulada por Christian Goldbach en 1742 en una célebre carta a
+--Euler. Es uno de los problemas abiertos (no resuelto) más antiguo de las Matemáticas y dice:
+--Todo número par mayor que 2 puede escribirse como la suma de dos primos.
+--Los dos primos no tienen que ser distintos. La conjetura ha sido comprobada con programas de
+--ordenador para todos los pares menores a 1018, pero no ha podido ser demostrada. El objetivo de
+--este ejercicio es comprobarla con Haskell.
+
+--a) Usando una lista por comprensión y las funciones del ejercicio anterior, define una función
+--que tome como parámetro un entero n y devuelva una lista de tuplas con todos los pares de primos
+--que suman n. Por ejemplo:
+--(observa que los pares no aparecen duplicados: por ejemplo, el par (29,11) no aparece en la lista
+--anterior).
+
+
+
+--b) Define una función que tome como parámetro un entero n y devuelva si n es un
+--entero par mayor que 2 y además existe al menos una pareja de primos que sume n. Por ejemplo:
+--Para resolver este apartado puedes utilizar la función predefinida , que
+--toma una lista y devuelve si es vacía:
+--Para comprobar la conjetura, define una función que tome como parámetro
+--un entero n y que devuelva si para todos los números pares mayores que 2 y menores o
+--iguales a n se cumple la conjetura. Por ejemplo: 
+--Ayuda: usa listas por comprensión y la función predefinida que toma
+--una lista de booleanos y devuelve si todos son ciertos.
+--Existe una forma más débil de la conjetura, llamada Conjetura débil de Goldbach (CDG): todo impar
+--mayor que 5 es suma de tres números primos. Si n es impar >5, entonces n-3 es par >2, y según la
+--CG, es suma de dos primos, de donde por ser 3 un primo, n será suma de tres primos; de aquí que
+--CG asegure CDG; sin embargo, CDG tampoco ha sido demostrada hasta la fecha. Define una nueva
+--función para comprobar esta forma de la conjetura.
+
+
+
+
+
+
+
+
+--------------------------------------------------------------------------------------------------------
+--10. Los factores propios de un número x son los divisores naturales de x estrictamente menores a x. Un
+---número natural es perfecto si coincide con la suma de sus factores propios.
+--a) Escribe una función para comprobar si un número es perfecto. Por ejemplo:
+
+--b) Escribe otra función que devuelva una lista con los números perfectos menores o iguales a un valor
+--dado. Por ejemplo:
